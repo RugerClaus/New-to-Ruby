9bb12484f3f9a0749ce992bad12b1aca22d41dcc
RugerClaus <rogerrock12games@gmail.com>
2022-08-08 16:33:56 -0700

    commit 7

--- RushTrivia0.0.2.rb
+++ RushTrivia0.0.2.rb
@@ -7,101 +7,92 @@ def rush_trivia
         rush_trivia()
     end
 
-    q_and_a = ["What is the name of Rush's original drummer?",
-    "What year did the album 'Hemispheres' release?","What year did the band form?",
-    "What is Alex Lifeson's real last name?","What is the name of the band's 1991 Album, featuring a rap by Geddy Lee?"]
+        q_and_a = ["John Rutsey","1978","1969","Zivojinovic","Roll The Bones","What is the name of Rush's original drummer?",
+        "What year did the album 'Hemispheres' release?","What year did the band form?",
+        "What is Alex Lifeson's real last name?","What is the name of the band's 1991 Album, featuring a rap by Geddy Lee?"]
 
-    answers_one = ['John Bonham', 'James Franco', 'Billie Jean', 'John Rutsey']
-    answers_two = ['1994', '1873', '1978', '1492']
-    answers_three = ['1960', '1873','1999', '1969']
-    answers_four = ['Jackson', 'Zivojinovic', 'Ryan', 'Kolishnakov']
-    answers_five = ['Hemispheres', 'Roll The Bones', 'Let It Be', 'Moving Pictures']
-    numbers_of_questions = ["(1): ", "(2): ", "(3): ", "(4): "]
-    
     puts 'Welcome to Rush Trivia'
-    puts 'press enter to begin'
+    puts 'Press enter/return to begin'
     input = gets
-    def questions()
-        i = 0
-        while i <= 5
-            i += 1
-            case i
-        when i == 1 then
-                puts q_and_a[0]
-                puts "#{numbers_of_questions[0]} #{answers_one[0]}"
-                puts "#{numbers_of_questions[1]} #{answers_one[1]}"
-                puts "#{numbers_of_questions[2]} #{answers_one[2]}"
-                puts "#{numbers_of_questions[3]} #{answers_one[3]}"
-                input = gets.chomp()
-                if input == '4'
-                    puts 'Correct'
-                    puts "\e[H\e[2J"
-                else
-                    puts '#####  Final Score: ' + String(x-1) + '  #####'
-                    endgame
-                end
-                return
-        when i == 2 then
-                puts "#{numbers_of_questions[0]} #{answers_two[0]}"
-                puts "#{numbers_of_questions[1]} #{answers_two[1]}"
-                puts "#{numbers_of_questions[2]} #{answers_two[2]}"
-                puts "#{numbers_of_questions[3]} #{answers_two[3]}"
-                if input == '3'
-                    puts 'Correct'
-                    puts "\e[H\e[2J"
-                else
-                    puts '#####  Final Score: ' + String(x-1) + '  #####'
-                    endgame
-                end
-                return
-        when i == 3 then
-                puts "#{numbers_of_questions[0]} #{answers_three[0]}"
-                puts "#{numbers_of_questions[1]} #{answers_three[1]}"
-                puts "#{numbers_of_questions[2]} #{answers_three[2]}"
-                puts "#{numbers_of_questions[3]} #{answers_three[3]}"
-                if input == '4'
-                    puts 'Correct'
-                    puts "\e[H\e[2J"
-                else
-                    puts '#####  Final Score: ' + String(x-1) + '  #####'
-                    endgame
-                end
-                return
-        when i == 4 then
-                puts "#{numbers_of_questions[0]} #{answers_four[0]}"
-                puts "#{numbers_of_questions[1]} #{answers_four[1]}"
-                puts "#{numbers_of_questions[2]} #{answers_four[2]}"
-                puts "#{numbers_of_questions[3]} #{answers_four[3]}"
-                if input == '2'
-                    puts 'Correct'
-                    puts "\e[H\e[2J"
-                else
-                    puts '#####  Final Score: ' + String(x-1) + '  #####'
-                    endgame
-                end
-                return
-        when i == 5 then
-                
-                puts "#{numbers_of_questions[0]} #{answers_five[0]}"
-                puts "#{numbers_of_questions[1]} #{answers_five[1]}"
-                puts "#{numbers_of_questions[2]} #{answers_five[2]}"
-                puts "#{numbers_of_questions[3]} #{answers_five[3]}"
-                if input == '2'
-                    puts 'Correct'
-                    puts "\e[H\e[2J"
-                else
-                    puts '#####  Final Score: ' + String(x-1) + '  #####'
-                    endgame
-                end
-                return
+    x = 0
+    
+    
+
+    # using for loop with the range
+    for questions in 1..5 do
+        
+        x += 1
+        puts 'Loading'
+        sleep 2
+        puts "\e[H\e[2J"
+        if x == 1
+            puts "\e[H\e[2J"
+            puts "Score: #{x-1}"
+            puts q_and_a[5]
+            input = gets.chomp()
+            if input == q_and_a[0]
+                puts 'Correct'
+                puts "\e[H\e[2J"
+            else
+                puts '#####  Final Score: ' + String(x-1) + '  #####'
+                endgame
             end
+            
         end
-    end
-    if input == ''
-        puts 'hello'
-        sleep 5
-    else
-        questions
+        if x == 2
+            puts 'Score: ' + String(x-1)
+            puts q_and_a[6]
+            input = gets.chomp()
+            if input == q_and_a[1]
+                puts 'Correct'
+                puts "\e[H\e[2J"
+            else
+                puts '#####  Final Score: ' + String(x-1) + '  #####'
+                endgame
+            end
+            
+        end
+        if x == 3
+            puts 'Score: ' + String(x-1)
+            puts q_and_a[7]
+            input = gets.chomp()
+            if input == q_and_a[2]
+                puts 'Correct'
+                puts "\e[H\e[2J"
+            else
+                puts '#####  Final Score: ' + String(x-1) + '  #####'
+                endgame
+            end
+            
+        end
+        if x == 4
+            puts 'Score: ' + String(x-1)
+            puts q_and_a[8]
+            input = gets.chomp()
+            if input == q_and_a[3]
+                puts 'Correct'
+                puts "\e[H\e[2J"
+            else
+                puts '#####  Final Score: ' + String(x-1) + '  #####'
+                endgame
+            end
+            
+        end
+        if x == 5
+            puts 'Score: ' + String(x-1)
+            puts q_and_a[9]
+            input = gets.chomp()
+            if input == q_and_a[4]
+                puts "\e[H\e[2J"
+                puts 'Correct'
+                puts 'You Win!'
+                puts '#####  Final Score: ' + String(x) + '  #####'
+            else
+                puts '#####  Final Score: ' + String(x-1) + '  #####'
+                endgame
+            end
+            
+        end
     end
 end
 begin
