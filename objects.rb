20540eb4e077a6c4ec162f61196440ccbf6b6c75
RugerClaus <rogerrock12games@gmail.com>
2022-08-07 21:31:41 -0700

    Another Commit

---
+++ objects.rb
@@ -0,0 +1,9 @@
+
+
+puts 'please enter a number to multiply'
+input1 = gets.chomp()
+puts 'please enter a second number'
+input2 = gets.chomp()
+solution = input1 * input2
+puts "Solution: #{solution}"
+sleep(4)
